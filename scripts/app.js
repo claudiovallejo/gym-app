56861c03bff969f92c2491c4582da31c5e040866
Claudio Vallejo <hola@claudiovallejo.mx>
2017-03-08 23:34:29 -0600

    Updated transition delay constant.

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,7 @@ $cards && window.addEventListener('load', loadCards);
 //
 function loadCards() {
   for (var i = 0; i < $cards.length; i++) {
-    $cards[i].setAttribute('style', 'transition-delay: ' + ((i+1)*0.075) + 's;');
+    $cards[i].setAttribute('style', 'transition-delay: ' + ((i+1)*0.15) + 's;');
     toggle($cards[i], 'o0', 'o1');
     toggle($cards[i], 'transform-tY0', 'transform-tYm4');
   };
