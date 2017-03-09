287f844821b0fe81dbd2d0f6f2ed39eed5d63354
Claudio Vallejo <hola@claudiovallejo.mx>
2017-03-08 23:08:54 -0600

    Initialized project; created sample gym routine.

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+const $cards = document.getElementsByClassName('js-card');
+$cards && window.addEventListener('load', loadCards);
+//
+function loadCards() {
+  for (var i = 0; i < $cards.length; i++) {
+    $cards[i].setAttribute('style', 'transition-delay: ' + (i*0.15) + 's;');
+    toggle($cards[i], 'o0', 'o1');
+    toggle($cards[i], 'transform-tY0', 'transform-tYm3');
+  };
+}
+//
+function toggle($element, classA, classB) {
+  $element.classList.toggle(classA);
+  $element.classList.toggle(classB);
+}
+const $total = document.querySelector(`[data-app="total"]`);
+$total.textContent = ($cards.length - 1) * 2 + " Exercises";
