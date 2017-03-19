484125d3d530085490d5094b74374c53f2ff5b59
Claudio <hola@claudiovallejo.mx>
2017-03-19 12:42:38 -0500

    Abstracted load elements function; Created new script to fade-in routine cards on load.

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,14 +1,22 @@
+//  Daily routine exercise cards
 const $cards = document.querySelectorAll('[data-value]');
-$cards && window.addEventListener('load', loadCards);
-//
-function loadCards() {
-  for (var i = 0; i < $cards.length; i++) {
-    $cards[i].setAttribute('style', 'transition-delay: ' + ((i+1)*0.15) + 's;');
-    toggle($cards[i], 'o0', 'o1');
-    toggle($cards[i], 'transform-tY0', 'transform-tYm4');
+$cards && window.addEventListener('load', function(){
+  loadElements($cards);
+});
+//  Weekly routine cards
+const $routines = document.querySelectorAll(`[data-card="routine"]`);
+$routines && window.addEventListener('load', function(){
+  loadElements($routines);
+});
+//  Prepare element cards for load animation
+function loadElements($elements) {
+  for (var i = 0; i < $elements.length; i++) {
+    $elements[i].setAttribute('style', 'transition-delay: ' + ((i+1)*0.15) + 's;');
+    toggle($elements[i], 'o0', 'o1');
+    toggle($elements[i], 'transform-tY0', 'transform-tYm4');
   };
 }
-//
+//  Toggle element classes
 function toggle($element, classA, classB) {
   $element.classList.toggle(classA);
   $element.classList.toggle(classB);
