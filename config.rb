8d51f9de8c2a6eca83b6d291b44bb61001bf66d8
Claudio <hola@claudiovallejo.mx>
2017-03-18 23:44:35 -0500

    Started working on cofigurating dynamic content integration from database.

--- config.rb
+++ config.rb
@@ -10,11 +10,12 @@ page '/*.json', layout: false
 page '/*.txt', layout: false
 
 # With alternative layout
-page "index.html", layout: :main
+page "routine.html", layout: :main
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+# Proxies
+data.routines.each do |routine|
+  proxy "/#{routine.day.parameterize}/index.html", "/routine.html", :locals => { :routine => routine }, :ignore => true
+end
 
 # General configuration
 
