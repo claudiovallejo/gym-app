0e087dbb5998d7bebf6e6eca54b0927d796099e3
Claudio Vallejo <hola@claudiovallejo.mx>
2017-04-18 19:09:04 -0500

    Reincorporated delayed component animations.

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,3 +1,21 @@
+//  Daily routine exercise cards
+const $cards = document.querySelectorAll('[data-value]');
+$cards && window.addEventListener('load', function(){
+  loadElements($cards);
+});
+//  Weekly routine cards
+const $routines = document.querySelectorAll('[data-card="routine"]');
+$routines && window.addEventListener('load', function(){
+  loadElements($routines);
+});
+//  Prepare element cards for load animation
+function loadElements($elements) {
+  for (var i = 0; i < $elements.length; i++) {
+    $elements[i].setAttribute('style', 'transition-delay: ' + ((i+1)*0.15) + 's;');
+    toggle($elements[i], ['o-0', 'o-1']);
+    toggle($elements[i], ['t-translateY0', 't-translateY4r']);
+  };
+}
 //  Preload test
 const $preload = document.getElementsByClassName('preload');
 window.addEventListener("load", function(){
